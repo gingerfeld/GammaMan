1869303ac488e60b64af5c6f696548554313d9f3
Daniel <gingerfeld@gmail.com>
2017-01-05 23:11:23 -0800

    Add 'Gamma Man/'

---
+++ Gamma Man/Assets/Scripts/Inventory/InventoryStorage.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class InventoryStorage : MonoBehaviour {
+    public List<item> inv = new List<item>();
+   public float gridSize;
+   
+   
+   public void AddItemToInventory(item i, coord grabOffset, coord dropPos){
+        if(FitsInInventory(i, grabOffset,dropPos)){
+            
+        }
+   }
+    
+    public bool FitsInInventory(item potentialFit, coord grabOffset, coord dropPos){
+        foreach(coord potentialFitCoord in potentialFit.coordinates){
+            coord invCoord = potentialFitCoord  + dropPos;
+        }
+        
+        
+        foreach(item otherItem in inv){
+            foreach(coord otherCoord in otherItem.coordinates){
+                coord otherCoord 
+                foreach(coord potentialFitCoord in potentialFit.coordinates){
+                    if(otherCoord.Equals(c)){
+                        return false;
+                    }
+                }                        
+            }
+        }
+        return true;
+    }
+   
+}
+
+[System.Serializable]
+public class item{
+    public string name;
+    public string description;
+    
+    public GameObject worldObject;
+    public Texture2D icon;
+    
+    public coord[] coordinates;
+    public coord invPosition;
+}
+
+public class coord{
+    public int x;
+    public int y;
+    public coord(int _x, int _y){
+        x = _x;
+        y = _y;
+    }
+    public static coord operator +(coord x, coord y){
+        return new coord(x.x+y.x,x.y+y.y);
+    }
+    public static coord operator -(coord x, coord y){
+        return new coord(x.x-y.x,x.y-y.y);
+    }
+    
+    
+    
+}
---
+++ Gamma Man/Assets/Scripts/UI/UIOpener.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UIOpener : MonoBehaviour {
+    public GameObject UICanvas;
+    
+    Transform invMenu;
+    Transform gameMenu;
+    
+    void Start(){
+        invMenu = UICanvas.transform.FindChild("invMenu");
+        gameMenu = UICanvas.transform.FindChild("gameMenu");
+    }
+    
+    void Update(){
+        if(Input.GetButtonDown("Menu")){
+                if(gameMenu.gameObject.activeSelf){
+                Time.timeScale = 1.0f;
+                gameMenu.gameObject.SetActive(false);
+                print("Closing menu");
+            }else{
+                Time.timeScale = 0.0f;
+                gameMenu.gameObject.SetActive(true);
+                print("Opening menu");
+            }
+        }else if (Input.GetButtonDown("Inventory")){
+            if(invMenu.gameObject.activeSelf){
+                Time.timeScale = 1.0f;
+                invMenu.gameObject.SetActive(false);
+                print("Closing inventory");
+            }else{
+                Time.timeScale = 0.0f;
+                invMenu.gameObject.SetActive(true);
+                print("Opening inventory");
+            }
+        }
+    }
+}
